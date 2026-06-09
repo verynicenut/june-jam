953bb55beb3a7a8fa0f0ad27f0a6950e864d3c5d
clarice <leeclarice10@gmail.com>
2026-06-09 02:55:28 +0000

    git push

---
+++ program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace JuneJam2026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+  
+        }
+    }
+}
